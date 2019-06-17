e5ec556ed100eb3574776e36a4b5e12abf35bf42
skarl9 <50229021+skarl9@users.noreply.github.com>
2019-06-17 16:59:36 +0200

    Add files via upload

---
+++ stm32_default_26MHz_V1.24.1/Inc/main.h
@@ -0,0 +1,106 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define Vent1_Pin GPIO_PIN_1
+#define Vent1_GPIO_Port GPIOA
+#define Vent2_Pin GPIO_PIN_2
+#define Vent2_GPIO_Port GPIOA
+#define Temp_Analog_Pin GPIO_PIN_3
+#define Temp_Analog_GPIO_Port GPIOA
+#define ESD_Analog_M5_Pin GPIO_PIN_4
+#define ESD_Analog_M5_GPIO_Port GPIOA
+#define ESD_Analog_M12_Pin GPIO_PIN_5
+#define ESD_Analog_M12_GPIO_Port GPIOA
+#define ESD_Analog_M19_Pin GPIO_PIN_6
+#define ESD_Analog_M19_GPIO_Port GPIOA
+#define ESD_Analog_M22_Pin GPIO_PIN_7
+#define ESD_Analog_M22_GPIO_Port GPIOA
+#define ESD_Analog_M48_Pin GPIO_PIN_4
+#define ESD_Analog_M48_GPIO_Port GPIOC
+#define ESD_Analog_MNETZ_Pin GPIO_PIN_5
+#define ESD_Analog_MNETZ_GPIO_Port GPIOC
+#define LED3_Pin GPIO_PIN_12
+#define LED3_GPIO_Port GPIOB
+#define LED2_Pin GPIO_PIN_13
+#define LED2_GPIO_Port GPIOB
+#define LED1_Pin GPIO_PIN_14
+#define LED1_GPIO_Port GPIOB
+#define INH_12_Pin GPIO_PIN_8
+#define INH_12_GPIO_Port GPIOA
+#define INH_19_Pin GPIO_PIN_9
+#define INH_19_GPIO_Port GPIOA
+#define ESD_Analog_A48_Pin GPIO_PIN_15
+#define ESD_Analog_A48_GPIO_Port GPIOA
+#define SD_Input_Pin GPIO_PIN_3
+#define SD_Input_GPIO_Port GPIOB
+#define MPU_INT_Pin GPIO_PIN_5
+#define MPU_INT_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
